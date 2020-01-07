f1b9027721a0f2340f02f886b3c67fd77de673ce
Tanis <tanishehe@gmail.com>
2020-01-06 21:03:07 -0600

    news.php

---
+++ news.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RPG Organizer</title>
+</head>
+<body>
+	<?php
+	$section = "news";
+	require("nav-bar.php");
+	?>
+	<p>
+		We are a website!
+	</p>
+</body>
+</html>
