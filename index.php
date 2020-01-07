81adde307f28438705b84b9b1a32bebb6b7ca0b6
Tanis <tanishehe@gmail.com>
2020-01-06 20:33:49 -0600

    index is getting pretty

--- index.php
+++ index.php
@@ -9,5 +9,38 @@
 <input type=button onClick="location.href='login.php'" value='Login'>
 <input type=button onClick="location.href='register.php'" value='Register'>
 
+<style>
+	.topnav {
+		background-color: #D2B48C;
+		overflow: hidden;
+	}
+	.topnav a {
+		float: left;
+		color: $fff;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+		font-size: 17px;
+	}
+	.topnav a:hover{
+		background-color: #ddd;
+		color: black;
+	}
+
+	.topnav a.active {
+		background-color: #604b2f;
+		color: white;
+	}
+</style>
+
+	<div class="topnav">
+		<a class="active" href="#home">Home</a>
+		<a href="#news">News</a>
+		<a href="#contact">Contact</a>
+		<a href="$about">About</a>
+	</div>
+
+
+
 </body>
 </html>
