015447da8f47b9bc70377840bb4c5db4338dea89
Tanis <tanishehe@gmail.com>
2020-01-06 21:11:11 -0600

    ahhh

--- index.php
+++ index.php
@@ -9,7 +9,14 @@
 	$section = "home";
 	require("nav-bar.php");
 ?>
-
+<img class="titlecard" src="titlecard.png"/>
+<style>
+	.titlecard {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+		width: 50%;
+	}
 
 </body>
 </html>
