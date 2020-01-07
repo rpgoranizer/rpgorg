f7b8c88340708779f8e6502c55191545311975f4
Tanis <tanishehe@gmail.com>
2020-01-07 17:12:45 -0600

    test

--- config.php
+++ config.php
@@ -1,13 +1,16 @@
 <?php
-
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'Jashon123!');
 define('DB_NAME', 'rpgorg');
-
+ 
+/* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if(link === false){
-	die("ERROR: Could Not Connect. " . mysqli_connect_error());
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
